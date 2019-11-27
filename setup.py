5225ae37fa73515d0ec409ded150409c2c0b5091
sashacmc <Alexander@Bushnev.ru>
2019-11-27 23:52:37 +0100

    Python/Debian packages and systemd service added.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+setup(name='photo-importer',
+      version='1.0.0',
+      description='Photo importer tool',
+      author='Alexander Bushnev',
+      author_email='Alexander@Bushnev.ru',
+      license='GNU General Public License v3.0',
+      packages=['photo_importer'],
+      data_files=[('/etc/',
+                   ['photo-importer.cfg']),
+                  ('/lib/systemd/system/',
+                   ['photo-importer.service']),
+                  ('share/photo-importer/web/',
+                   ['web/index.html', 'web/log.png'])],
+      scripts=['photo-importer', 'photo-importer-server'],
+      zip_safe=False)
