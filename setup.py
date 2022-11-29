3fa90fa913d9801cd69ee46c31429ed8a29b728a
Alexander Bushnev <Alexander@Bushnev.pro>
2022-11-29 09:07:14 +0100

    Update version/changelog

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_long_description():
 
 setup(
     name='photo-importer',
-    version='1.1.2',
+    version='1.2.0',
     description='Photo importer tool',
     author='Alexander Bushnev',
     author_email='Alexander@Bushnev.pro',
