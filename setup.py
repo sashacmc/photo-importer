b75bd8d1d5fb6bff8310fd1e60e74f57443281cd
sashacmc <Alexander@Bushnev.pro>
2024-07-09 00:58:56 +0200

    Version 1.2.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_long_description():
 
 setup(
     name='photo-importer',
-    version='1.2.4',
+    version='1.2.5',
     description='Photo importer tool',
     author='Alexander Bushnev',
     author_email='Alexander@Bushnev.pro',
